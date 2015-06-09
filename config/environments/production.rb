2c5f9241c5748ed5d3e7c0612a5725c9a1827310
maxpospelov <maxpospelov@mail.ru>
2015-06-09 14:51:06 +0500

    Precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Shop::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb', '*.jpeg', '*.jpg', '*.png', '*.gif']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
