8958ab2a06ff3519251cc0fae9625d7b0cd23b7b
maxpospelov <maxpospelov@mail.ru>
2015-06-11 08:32:54 +0500

    add S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -33,12 +33,13 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.root = Rails.root.join('tmp')
     config.cache_dir = 'carrierwave'
-    
+    config.storage = 's3'
+
     config.fog_credentials = {
         :provider               => 'AWS',                        # required
         :aws_access_key_id      => 'AKIAIT25W2UA7RFIMWAQ',                        # required
         :aws_secret_access_key  => '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',                     # required
     }
-    config.fog_directory  = 'fringoo'                             # required
+    config.fog_directory  = 'production'                             # required
   end
 end
