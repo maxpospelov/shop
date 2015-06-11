9380244b92a9715a7b5c8413bbab6827eed4cfeb
maxpospelov <maxpospelov@mail.ru>
2015-06-11 13:15:17 +0500

    Asset

--- app/uploaders/product_uploader.rb
+++ app/uploaders/product_uploader.rb
@@ -9,7 +9,7 @@ class ProductUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   #storage :file
 
-  
+
   if Rails.env.production?
     storage :dropbox
   else
